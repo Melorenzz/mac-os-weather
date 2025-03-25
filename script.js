5511d4209218e948153c15509117f91923f33977
MEL0RENZ <gomisha552@gmail.com>
2025-03-25 16:09:00 +0100

    Added custom wweather background

--- script.js
+++ script.js
@@ -44,11 +44,29 @@ const getWeatherData = () => {
             document.querySelectorAll('.temperature').forEach((item) => {
                 item.innerText = data.main.temp + '°';
             })
+            const icon = data.weather[0].icon; // Иконка погоды
+            const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
             document.getElementById('description').innerText = data.weather[0].description;
+            document.getElementById('weatherIcon').innerHTML = ` <img src=${iconUrl}> `;
             document.getElementById('userLocationMap').innerHTML = `
                 <div style="width: 100%; height: 100%"><iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=${q}&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/collections/drones/">gps drone</a></iframe></div>
             `
+            setBackground(data.weather[0].description);
+            console.log(data.weather[0].description)
         })
 }
 getWeatherData()
 
+function setBackground(description){
+    switch (description){
+        case 'scattered clouds':
+            document.getElementById('bgWeather').style.backgroundImage = `url('./img/descriptionIMG/${description}.png')`;
+            break;
+        case 'mist':
+            document.getElementById('bgWeather').style.backgroundImage = `url('./img/descriptionIMG/${description}.png')`;
+            break;
+        case 'overcast clouds':
+            document.getElementById('bgWeather').style.backgroundImage = `url('./img/descriptionIMG/${description}.png')`;
+            break;
+    }
+}
